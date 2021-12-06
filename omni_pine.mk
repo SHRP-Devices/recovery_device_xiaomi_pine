d939b8946601d04cc5fd4207ca536ff66cb5034e
Atharv <81741009+atharv2951@users.noreply.github.com>
2021-12-06 22:19:43 +0530

    Add files via upload

---
+++ omni_pine.mk
@@ -0,0 +1,17 @@
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from those products. Most specific first.
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)  # If you are building for a tablet
+
+## Device identifier. This must come after all inclusions
+PRODUCT_RELEASE_NAME := pine
+
+PRODUCT_NAME := omni_pine
+PRODUCT_DEVICE := pine
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := Redmi 7A

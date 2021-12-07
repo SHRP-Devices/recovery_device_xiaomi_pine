2ce3406b73a79d9868a367aa667dd3f41413d193
Atharv <81741009+atharv2951@users.noreply.github.com>
2021-12-07 08:25:47 +0530

    Update and rename omni_pine.mk to twrp_pine.mk

--- twrp_pine.mk
+++ twrp_pine.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from those products. Most specific first.
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)  # If you are building for a tablet
@@ -10,7 +10,7 @@ $(call inherit-product, vendor/omni/config/common.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_RELEASE_NAME := pine
 
-PRODUCT_NAME := omni_pine
+PRODUCT_NAME := twrp_pine
 PRODUCT_DEVICE := pine
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
